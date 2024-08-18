bed120136a1707881b71bc5241534b0ab924964a
frufrok <frufrok@gmail.com>
2024-08-19 00:51:40 +0300

    Добавлены Unit-тесты для RestrictedController.

---
+++ UserAPITest/RestrictedControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserAPI.Authorization;
+using UserAPI.Controllers;
+using UserAPI.Models.DTO;
+
+namespace UserAPITest
+{
+    public class RestrictedControllerTest
+    {
+        private RestrictedController _controller;
+        private Guid testGuid = Guid.NewGuid();
+
+        [SetUp]
+        public void Setup()
+        {
+            var mock = new Mock<IControllerUserSource>();
+            mock.SetReturnsDefault<UserDto>(new UserDto() { Guid = testGuid });
+            _controller = new RestrictedController(mock.Object);
+        }
+
+
+        [Test]
+        public void GetIdFromTokenTest()
+        {
+            var result = (ObjectResult)_controller.GetIdFromToken();
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+                Assert.That(result.Value, Is.EqualTo(testGuid));
+            });
+        }
+    }
+}
